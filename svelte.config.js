325946faa8e94ba22afac61bb2ea3b48f801c2a2
skvshetty <suhaskumarvs@yahoo.com>
2021-12-19 14:41:26 +0000

    fixed port issue

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		vite: {
 			server: {
 				hmr: {
-					clientPort: process.HMR_HOST ? 443: 3000,
+					clientPort: process.HMR_HOST ? 443 : 3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
 				}
 			}
